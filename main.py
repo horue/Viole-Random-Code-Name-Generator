edbcca1475344eddc8fe876d517730483d03ffee
Jorge <jorgejapao00@gmail.com>
2024-05-08 08:13:28 -0300

    Function for tests created

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 from generator import viole_rcng as viole
 
 
+def test():
+    print('-------------------------TESTS------------------------------')
 
-print('-------------------------TESTS------------------------------')
+    for i in range(5):
+        test=viole.gencode(name=viole.gencode())
+        print(test)
 
-for i in range(5):
-    test=viole.gencode(name=viole.gencode())
-    print(test)
+    print(60*'-')
 
-print(60*'-')
+    for i in range(5):
+        test=viole.gencode()
+        print(test)
 
-for i in range(5):
-    test=viole.gencode()
-    print(test)
+    print(60*'-')
 
-print(60*'-')
+    for i in range(5):
+        test=viole.gencode(x=3)
+        print(test)
 
-for i in range(5):
-    test=viole.gencode(x=3)
-    print(test)
+    print(60*'-')
 
-print(60*'-')
+    for i in range(5):
+        test=viole.gencode(name='Testin-Name')
+        print(test)
 
-for i in range(5):
-    test=viole.gencode(name='Testin-Name')
-    print(test)
+test()
